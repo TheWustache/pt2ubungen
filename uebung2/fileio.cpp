ff23463f01676be6696650e2853fdc27132009b1
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2017-05-15 16:48:32 +0200

    fileio changed, error is messed up but regex may be fine

--- uebung2/fileio.cpp
+++ uebung2/fileio.cpp
@@ -16,10 +16,15 @@ bool isValueCorrect(const std::string &teststring, const int &column)
 		/*case 1:
 			regExp = "[a-zA-Z]+";
 			break;*/
-	case 5:
-		regExp = "|{3,4}[a-zA-Z0-9äöüÄÖÜß]";
+	case 5:	//empty string or 3-4 Letter code
+		regExp = "([]*)|([a-zA-Z0-9äöüÄÖÜß]{3,4})";
 		break;
-
+	case 8: // 0 - 29999
+		regExp = "[0-2]?[0-9]{1,4}";
+		break;
+	case 10: //
+		regExp = "[EASOZNU]{1}";
+		break;
 		// Todo: implement cases for other columns
 
 		default:
@@ -34,20 +39,32 @@ void readTokensAndLines(char* path)
 {
 	std::ifstream file(path);
 	std::string parsed, line;
-
+	std::ofstream errorLog;
 	while (std::getline(file, line)) {
 		std::istringstream linestream;
 		linestream.str(line);
-		std::string field;
-
+		
 		std::vector<std::string> savedLine;
-		//with ',' as delim		
-		while (std::getline(linestream, field, ',')) {
-			savedLine.push_back(field);
+		
+		while (std::getline(linestream, parsed, ',')) {
+			
+			
+			savedLine.push_back(parsed);
+			
 		}
-		if (savedLine.size() < 12) //Adding "nothing" for missing timezones
+		//Adding "nothing" for missing timezones since its the end of the line
+		if (savedLine.size() < 12) 
 			savedLine.push_back("");
 		std::cout << savedLine.at(1) << " - " << savedLine.at(11) <<std::endl;
+
+		//write Error, Works but other stuff didnt
+
+		/*if (isError) {
+			if (!errorLog.is_open()) {
+				errorLog.open("fileio.log");
+			}
+			errorLog << line << "  -  Error at Field:" << errorCode << std::endl;
+		}*/
 
 		savedLine.clear();
 		// Todo: - Split line and write result to std::cout
@@ -56,6 +73,7 @@ void readTokensAndLines(char* path)
 
 
 	}
+		if (errorLog.is_open())errorLog.close();
 }
 
 int main(int argc, char * argv[])
