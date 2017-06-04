a65b23172c4807435e99879945b6a8b4fb7d95b5
TheWustache <leonard.geier@hotmial.com>
2017-06-04 16:28:22 +0200

    exceptions.cpp

--- uebung3/exceptions.cpp
+++ uebung3/exceptions.cpp
@@ -38,13 +38,90 @@ void parseLine(std::string line, int lineNum)
 	// Catch all exceptions thrown by these methods.
 	// If there have been any exceptions, aggregate all necessary information into an instance of FormatException and throw that instance.
 
-
+    try {
+        // parse line
+        std::stringstream ss(line);
+        ss.exceptions(std::ios::failbit | std::ios::badbit);
+        std::string fields[3];
+        
+        for(int i=0; i<3; i++) {
+            getline(ss, fields[i], ';');
+        }
+        
+        // check fields
+        bool error = false;
+        FormatException e = {lineNum, ""};
+        
+        // Date
+        try {
+            stringToTime(fields[0]);
+        }
+        catch (std::logic_error &x) {
+            e.m_actFields += fieldNames[0];
+            error = true;
+        }
+        
+        // Temperature
+        try {
+            std::stof(fields[1]);
+        }
+        catch (std::logic_error &x) {
+            if(e.m_actFields != "") {
+                e.m_actFields += ", ";
+            }
+            e.m_actFields += fieldNames[1];
+            error = true;
+        }
+        
+        // Temperature
+        try {
+            std::stof(fields[2]);
+        }
+        catch (std::logic_error &x) {
+            if(e.m_actFields != "") {
+                e.m_actFields += ", ";
+            }
+            e.m_actFields += fieldNames[2];
+            error = true;
+        }
+        
+        if(error) {
+            throw e;
+        }
+    }
+    catch (std::ios::failure &fail) {
+        std::cout << "Error parsing stringstream: " << fail.what();
+    }
+    catch (FormatException &e) {
+        throw e;
+    }
 }
 
 // TODO 3.1d
 void writeOutFormatException(const FormatException& e)
 {
-
+    std::ofstream log;
+    static bool initialized = false;
+    
+    try {
+        if(!initialized) {
+            log.open("../log.txt", std::ios::trunc);
+            initialized = true;
+        }
+        else {
+            log.open("../log.txt", std::ios::app);
+        }
+        
+        log.exceptions(std::ios::failbit);
+        
+        log << e.m_actLine << ": " << e.m_actFields << std::endl;
+        
+        log.close();
+    }
+    catch (std::ios::failure &fail) {
+        std::cout << "Error opening/reading/closing log: " << fail.what() << std::endl;
+    }
+    
 }
 
 void checkData(std::string path)
@@ -54,11 +131,41 @@ void checkData(std::string path)
 	std::ifstream file;
 
 	// TODO 3.1a: open file + read each line + call parseLine function (catch ifstream::failure)
-	// TODO 3.1c: read each line + call parseLine function (catch FormatException) + count valid + invalid lines
+    try {
+        // open file
+        file.open(path);
+        file.exceptions(std::ios::failbit);
+    
+    
+        // TODO 3.1c: read each line + call parseLine function (catch FormatException) + count valid + invalid lines
 
+        // call parseLine for each line
+        std::string line;
+        int count = 0;
+        
+        // skip first line
+        std::getline(file, line);
+        
+        while(std::getline(file, line)) {
+            count++;
+            try {
+                parseLine(line, count);
+                validLines++;
+            }
+            catch (FormatException &e) {
+                invalidLines++;
+                writeOutFormatException(e);
+            }
+        }
 
-
-	std::cout << "valid data fields: " << validLines << " - invalid data fields: " << invalidLines << std::endl;
+        // close file
+        file.close();
+        
+        std::cout << "valid data fields: " << validLines << " - invalid data fields: " << invalidLines << std::endl;
+    }
+    catch (std::ios::failure &fail) {
+        std::cout << "Error opening/reading/closing file: " << fail.what() << std::endl;
+    }
 }
 
 int main(int argc, char * argv[])
