3bc134baa146c1058105fef5b1ff5215adf3ebd1
TheWustache <leonard.geier@hotmial.com>
2017-07-02 19:06:42 +0200

    schedule.cpp an neuen Programmrahmen angepasst

--- uebung5/schedule.cpp
+++ uebung5/schedule.cpp
@@ -1,4 +1,4 @@
-
+//
 #include <iostream>
 #include <time.h>
 #include <vector>
@@ -10,7 +10,7 @@ struct Interval
 	Interval(const int i, const int start, const int length)
 	: index(i)
 	, start(start)
-	, end(start + length)
+	, end(start + length - 1)
 	{
 	}
 
@@ -26,9 +26,7 @@ const static int N = 20;
 
 std::ostream & operator<<(std::ostream & os, const std::vector<Interval> & I)
 {
-    
 	// Todo 5.3: Implement a nice print function
-    
     for( int j=0; j < I.size(); j++ ) {
         // one line
         os << "#" << I[j].index << ( I[j].index < 10 ? "  " : " " ) << "|";
@@ -37,8 +35,7 @@ std::ostream & operator<<(std::ostream & os, const std::vector<Interval> & I)
         }
         os << "|" << std::endl;
     }
-    
-//	os << I.size() << std::endl;
+	//os << I.size() << std::endl;
 	return os;
 }
 
@@ -51,7 +48,7 @@ void randomize(std::vector<Interval> & intervals)
 	for (int i = 0; i < N; i++)
 	{
 		int duration = rand() % MaxDuration + 1;
-		int start = rand() % (MaxEnd - duration);
+		int start = rand() % (MaxEnd - duration + 2);
 
 		intervals.push_back(Interval(i, start, duration));
 	}
@@ -60,9 +57,10 @@ void randomize(std::vector<Interval> & intervals)
 void schedule(const std::vector<Interval> & intervals)
 {
 	std::cout << std::endl << "intervals (randomized):" << std::endl << intervals;
-    
+
+
 	// Todo 5.3: Sort intervals
-	auto sorted = intervals;
+    auto sorted = intervals;
     std::sort( sorted.begin(), sorted.end(), []( const Interval &a, const Interval &b ){ return a.end < b.end; });
     
     std::cout << std::endl << "intervals (sorted):" << std::endl << sorted;
