5cc83f938bc06f41444b96a544e24dfed3f2b2bf
TheWustache <leonard.geier@hotmial.com>
2017-07-28 00:31:23 +0200

    stampedset.cpp finished

--- uebung7/stampedset.cpp
+++ uebung7/stampedset.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <assert.h>
 
+#include <unordered_map>
+#include <list>
+#include <utility>
+#include <set>
+
 using namespace std;
 
 
@@ -10,63 +15,169 @@ public:
 	StampedSet() {}
 
 	stamp_type add(const T& obj) {
-		// TODO
+        stampToObj.insert( make_pair(curStamp, obj) );
+        // insert object if it hasn't been yet, otherwise add the stamp to its list
+        if( objToStamp.count(obj) ) {
+            objToStamp.at(obj).push_back(curStamp);
+        }
+        else {
+            objToStamp.insert( make_pair( obj, list<stamp_type>({curStamp}) ) );
+        }
+        
+        curStamp++;
+        return curStamp - 1;
 	}
 
 	void erase(const T& obj) {
-		// TODO
+        // only erase if obj is in StampedSet
+        if( objToStamp.count(obj) ) {
+            // needs to be done for both maps
+            for(auto it = stampToObj.begin(), ite = stampToObj.end(); it != ite;) {
+                if((*it).second == obj) {
+                    it = stampToObj.erase(it);
+                }
+                else {
+                    ++it;
+                }
+            }
+            
+            objToStamp.erase( objToStamp.find(obj) );
+        }
 	}
 
 	unsigned long noOfObjects() const {
-		// TODO
+        return objToStamp.size();
 	}
 
 	unsigned long noOfStamps() const {
-		// TODO
+        return stampToObj.size();
 	}
 
 	bool containsObject(const T& obj) const {
-		// TODO
+        return objToStamp.count(obj);
 	}
 
 	bool containsStamp(const stamp_type& s) const {
-		// TODO
+        return stampToObj.count(s);
 	}
 
 	const T& findObject(const stamp_type& s) const {
-		// TODO
+        assert( containsStamp(s) );
+        return stampToObj.at(s);
 	}
 
 	stamp_type lastStamp(const T& obj) const {
-		// TODO
+        assert( containsObject(obj) );
+        return objToStamp.at(obj).back();
 	}
 
 	stamp_type firstStamp(const T& obj) const {
-		// TODO
+        assert( containsObject(obj) );
+		return objToStamp.at(obj).front();
 	}
 
 	stamp_type nextStamp() const {
-		// TODO
+        return curStamp;
 	}
 
 	template<typename L>
 	void process(const T& obj, L&& fct) {
-		// TODO
-	}
+        assert( containsObject(obj) );
+        // function parameters should be 'T, stamp_type'
+        // do da processing
+        list<stamp_type>& stampList = objToStamp.at(obj);
+        for(auto it = stampList.begin(), ite = stampList.end(); it != ite;) {
+            if( fct(obj, *it) ) {
+                stampToObj.erase( stampToObj.find(*it) );
+                it = stampList.erase(it);
+            }
+            else {
+                ++it;
+            }
+        }
+        
+        // add stamp if all were removed
+        if( stampList.empty() ) {
+            add(obj);
+        }
+    }
 
 	template<typename L>
 	void process(stamp_type from, stamp_type to, L&& fct) {
-		// TODO
-	}
+        assert( from <= to );
+        // set for keeping track of all objects that were processed
+        set<T> processedObj;
+        // function parameters should be 'T, stamp_type'
+        // process dat stuff
+        for(stamp_type s = from; s <= to; s++) {
+            if( containsStamp(s) ) {
+                T& obj = stampToObj.at(s);
+                // only process each object once
+                if( !processedObj.count(obj) ) {
+                    // process all stamps of the object within the given range
+                    list<stamp_type>& stampList = objToStamp.at(obj);
+                    for(auto it = stampList.begin(), ite = stampList.end(); it != ite;) {
+                        // if the cuurent stamp is already bigger than to, all of the following will be too
+                        if(*it > to) {
+                            break;
+                        }
+                        if(*it >= from) {
+                            if( fct(obj, *it) ) {
+                                stampToObj.erase( stampToObj.find(*it) );
+                                it = stampList.erase(it);
+                            }
+                            else {
+                                ++it;
+                            }
+                        }
+                        else {
+                            ++it;
+                        }
+                    }
+                    // make sure obj has at least one stamp
+                    if( stampList.empty() ) {
+                        add(obj);
+                    }
+                }
+                processedObj.insert(obj);
+            }
+        }
+    }
 
 	stamp_type restart(const T& obj) {
-		// TODO
+        assert( containsObject(obj) );
+        erase(obj);
+        return add(obj);
 	}
 
 private:
-	// TODO
+    // two maps so both the stamp and the object can be used to look up each other
+    unordered_map<stamp_type, T> stampToObj;
+    unordered_map<T, list<stamp_type>> objToStamp;
+    
+    stamp_type curStamp = 0;
 };
 
+void testierTest() {
+    StampedSet<char, int> s;
+    char c = 'X';
+    char c2 = 'Y';
+    char c3 = 'Z';
+    
+    int x = s.add(c);
+    s.add(c2);
+    int start = s.add(c3);
+    s.add(c3);
+    s.add(c);
+    int fin = s.add(c);
+    s.add(c3);
+    
+    s.process( c, [](char obj, int s){cout << "Obj: " << obj << " Stamp: " << s << "\n"; return false;});
+    s.restart(c);
+    s.process( c, [](char obj, int s){cout << "Obj: " << obj << " Stamp: " << s << "\n"; return false;});
+    
+    std::cout << "prolly works\n";
+}
 
 void test() {
 	StampedSet<float, int> sfs;
@@ -119,5 +230,6 @@ void test() {
 
 int main(int,char**) {
 	test();
+//    testierTest();
 	return 0;
 }
